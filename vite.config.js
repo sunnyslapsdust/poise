4462ff80d1bb8ae48910b3f4e41dfa7268e32589
sunny <sunny.slaps.dust@gmail.com>
2026-03-17 20:53:34 +0800

    fix config for PWA

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
         background_color: '#0f1117',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/poise/',
+        start_url: '/poise/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
